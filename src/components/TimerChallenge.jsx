403ed9c7c85b8ff8c291c3b65bf225674c7ee78a
AnastasiosMpoletis <anastasiosmpoletis@gmail.com>
2025-03-23 22:55:35 +0200

    L-139. Create timer ref and implement stop functionality

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,17 +1,26 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 export default function TimerChallenge({ title, targetTime }) {
+    /**
+     * We use timer ref (and not a let) so that every TimerChallenge instance has its own timer and we do not have timer conflicts between instances.
+     * timer will not be reset or cleared when this component re-executes.
+     */
+    const timer = useRef();
 
     const [timerStarted, setTimerStarted] = useState(false);
     const [timerExpired, setTimerExpired] = useState(false);
 
     function handleStart() {
-        setTimeout(() => {
+        timer.current = setTimeout(() => {
             setTimerExpired(true);
         }, targetTime * 1000);
 
         // setTimerStarted will be executed when setTimeout was set (not after 1000ms)
         setTimerStarted(true);
+    }
+
+    function handleStop() {
+        clearTimeout(timer.current);
     }
 
     return (
@@ -22,7 +31,7 @@ export default function TimerChallenge({ title, targetTime }) {
                 {targetTime} second{targetTime > 1 ? "s" : ""}
             </p>
             <p>
-                <button onClick={handleStart}>
+                <button onClick={timerStarted ? handleStop : handleStart}>
                     {timerStarted ? "Stop" : "Start"} Challenge
                 </button>
             </p>
