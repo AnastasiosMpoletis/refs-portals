6a9bd19e870270dcfdf0a78036d28696f6f29cf7
AnastasiosMpoletis <anastasiosmpoletis@gmail.com>
2025-03-23 21:33:32 +0200

    L-134. Create an input ref that holds the input value and display the value in welcome message

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,22 @@
+import { useState, useRef } from "react";
+
 export default function Player() {
+  const playerName = useRef();
+  const [enteredPlayerName, setEnteredPlayerName] = useState(null);
+
+  function handleClick() {
+    // playerName is always a JavaScript object and playerName.current holds the component (the input in our case). Every ref has only one current
+    setEnteredPlayerName(playerName.current.value);
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      {/* JavaScript ?? operator -> if enteredPlayerName is truthy, return it or else return "unknown entity" */}
+      <h2>Welcome {enteredPlayerName ?? "unknown entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        {/* Connect ref to component with this ref={} special attribute */}
+        <input ref={playerName} type="text" />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
