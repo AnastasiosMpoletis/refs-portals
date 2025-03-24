56f243cbb0ffbc10d18e73069cbfe8584c2ddd8f
AnastasiosMpoletis <anastasiosmpoletis@gmail.com>
2025-03-24 11:54:12 +0200

    L-141. Add a forwardRef to ResultModal for older React version (prior to 19)

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,7 +1,8 @@
-export default function ResultModal({ result, targetTime }) {
+import { forwardRef } from 'react';
+
+const ResultModal = forwardRef(function ResultModal({ result, targetTime }, ref) {
     return (
-        // open attribute displays the dialog when called. If used, we cannot dim the background. We should use ref for that.
-        <dialog className="result-modal" open>
+        <dialog ref={ref} className="result-modal">
             <h2>Your {result}</h2>
             <p>
                 The target time was <strong>{targetTime} seconds.</strong>
@@ -15,4 +16,6 @@ export default function ResultModal({ result, targetTime }) {
             </form>
         </dialog>
     );
-}
+});
+
+export default ResultModal;
